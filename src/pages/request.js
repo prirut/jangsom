a69f27e729a96b969665631c4ac3ccf7cb5ecad4
prirut <prirut_27@hotmail.com>
2020-10-10 13:17:49 +0700

    image

--- src/pages/request.js
+++ src/pages/request.js
@@ -154,7 +154,6 @@ function Request() {
 
   function generateUUID() {
     // Public Domain/MIT
-    var d = new Date().getTime(); //Timestamp
     var d2 = (performance && performance.now && performance.now() * 1000) || 0; //Time in microseconds since page-load or 0 if unsupported
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (
       c
@@ -162,7 +161,7 @@ function Request() {
       var r = Math.random() * 16; //random number between 0 and 16
       if (d > 0) {
         //Use timestamp until depleted
-        r = (d + r) % 16 | 0;
+        r = (d2 + r) % 16 | 0;
         d = Math.floor(d / 16);
       } else {
         //Use microseconds since page-load if supported
